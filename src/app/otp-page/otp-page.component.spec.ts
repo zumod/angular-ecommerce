c32e70e795959f51cadde6114844e850fdac3736
Sumod Sivadas <contactsumod@gmail.com>
2020-08-03 21:31:50 +0530

    Added OTP Modules

---
+++ src/app/otp-page/otp-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OtpPageComponent } from './otp-page.component';
+
+describe('OtpPageComponent', () => {
+  let component: OtpPageComponent;
+  let fixture: ComponentFixture<OtpPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OtpPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OtpPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/otp-page/otp-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-otp-page',
+  templateUrl: './otp-page.component.html',
+  styleUrls: ['./otp-page.component.css']
+})
+export class OtpPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
